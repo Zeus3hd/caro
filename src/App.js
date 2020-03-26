8df691dc3606e07d3078b335f04ad404c563e472
Ahd <dafuq.ahd@gmail.com>
2020-03-26 03:02:33 +0300

    my cool animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
+import img1 from "./img/cat1.jpeg";
+import img2 from "./img/cat2.jpeg";
+import img3 from "./img/cat3.jpeg";
+import img4 from "./img/cat4.jpeg";
 
 function App() {
-	return <div className="App"></div>;
+	const [offset, setOffset] = useState(0);
+
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			setOffset(window.scrollY * -0.4);
+		});
+		let titles = document.querySelectorAll(".anim");
+		let observer = new IntersectionObserver(entries => {
+			console.log(entries);
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("animate");
+				} else {
+					entry.target.classList.remove("animate");
+				}
+			});
+		});
+		titles.forEach(title => {
+			observer.observe(title);
+		});
+	}, []);
+
+	return (
+		<div className="App">
+			<section>
+				<h1 className="anim">You have no idea how cool SVG is</h1>
+			</section>
+			<section>
+				<h1 className="anim">Animating SVG is so fucking fun</h1>
+			</section>
+			<section>
+				<h1 className="anim">
+					Seriously You <i>NEED</i> to try playing with SVG
+				</h1>
+			</section>
+			<svg viewBox="10 0 400 350">
+				<path
+					id="our-text"
+					fill="#fff"
+					d="M.756 295.488s104.737-30.504 70.618-124.363C37.255 77.265 208.643 2.179 208.643 2.179"
+				/>
+				<text y="40" fontSize="30" fontFamily="san-serif">
+					<textPath
+						id="text-path"
+						className="text"
+						href="#our-text"
+						startOffset={offset}
+					>
+						this is the new trick i learned today and it looks so goddamn cool
+					</textPath>
+				</text>
+			</svg>
+		</div>
+	);
 }
 
 export default App;
